a22f09b2c1cd1fd2f3e25466a455ff4263abc6b4
KacSzu <szulckacper19@gmail.com>
2024-01-22 17:56:00 +0100

    Implementing Slider

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -18,6 +18,10 @@ function ProductListing({ product, index }: ProductListingProps) {
     ({ value }) => value === product?.category
   )?.label;
 
+  const validUrls = product?.images
+    .map(({ image }) => (typeof image === "string" ? image : image.url))
+    .filter(Boolean);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(true);
@@ -35,7 +39,7 @@ function ProductListing({ product, index }: ProductListingProps) {
         href={`/product/${product.id}`}
       >
         <div className="flex flex-col w-full">
-          <ImageSlider />
+          <ImageSlider urls={validUrls} />
           <h3 className="mt-4 font-medium text-sm text-gray-700">
             {product.name}
           </h3>
